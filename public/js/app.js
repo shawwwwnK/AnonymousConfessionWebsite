b1adb33f9a604fbdc02e964206b93db0d3a6e727
Shawn Kang <kangzx@stanford.edu>
2023-06-09 10:50:03 -0700

    Initialize DB

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,23 @@
+import Filter from "./filter";
 
 export default class App {
     constructor() {
+        this._user = null;
+        this._filter = new Filter();
+
+        this._onGuestLogin = this._onGuestLogin.bind(this);
+        this._guestLoginButton = document.querySelector("#guestLogin");
+        this._onGoogleLogin = this._onGoogleLogin.bind(this);
+        this._googleLoginButton = document.querySelector("#googleLogin");
+    }
+
+
+    async _onGuestLogin() {
 
     }
 
-    
+    async _onGoogleLogin() {
+
+    }
+
 }
