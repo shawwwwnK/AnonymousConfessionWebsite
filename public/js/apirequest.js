9ba05a717033b1f39138346dee6fd337dbd7b2ec
Shawn Kang <kangzx@stanford.edu>
2023-06-09 00:58:16 -0700

    Structure

---
+++ public/js/apirequest.js
@@ -0,0 +1,48 @@
+API_URL = "/api";
+
+/* Subclass of Error for representing HTTP errors returned from the API.
+   Exposes a status (the HTTP response status code) and message (a user-facing message).
+
+   Example usage:
+      throw new HTTPError(500, "This feature is not implemented"); */
+export class HTTPError extends Error {
+  /* status is the HTTP status, message is a user-facing error message. */
+  constructor(status, message) {
+    /* Call the Error constructor with the given message. */
+    super(message);
+    this.status = status;
+  }
+}
+
+/* Make an API request.
+   - method is the HTTP method.
+   - path is the URI. It must begin with a /. Does not include API_URL.
+   - body (optional) is the request body as a JS object that can be converted to JSON.
+
+   The API is assumed to return JSON. If the response status is 200, the response body (as a JS object) is returned.
+   If the response has any other status, an HTTPError is thrown, with its status set to the response status and its
+   message set to the value of the `error` property of the response, which we assume is a user-facing error message. */
+const apiRequest = async (method, path, body = null) => {
+  // Set up path and options
+  let fullPath = API_URL + path;
+  let options = {
+    method: method,
+    headers: {"Content-Type": "application/json"},
+  }
+  if (body){
+    options.body = JSON.stringify(body);
+  }
+
+  // Send request and return json object or throw error
+  let res = await fetch(fullPath, options);
+  if (res.status === 200){
+    let responseBody = await res.json();
+    return responseBody;
+  }
+  else{
+    let errorResponse = await res.json();
+    throw new HTTPError(res.status, errorResponse.error);
+  }
+};
+
+export default apiRequest;
---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+import apiRequest, { HTTPError } from "./apirequest.js";
+import App from "./app.js";
+
+const testApp = () => {
+
+};
+window.testApp = testApp;
+
+const main = () => {
+    new App();
+};
+main();
