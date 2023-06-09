278cdcec94c8368f89bd01b4658cad2c29472287
Shawn Kang <kangzx@stanford.edu>
2023-06-09 12:59:25 -0700

    Construct post class

--- public/js/app.js
+++ public/js/app.js
@@ -6,11 +6,15 @@ export default class App {
     constructor() {
         this._user = null;
         this._filter = new Filter();
+        this._posts = [];
 
         this._onGuestLogin = this._onGuestLogin.bind(this);
-        this._guestLoginButton = document.querySelector("#guestLogin");
         this._onGoogleLogin = this._onGoogleLogin.bind(this);
+
+        this._guestLoginButton = document.querySelector("#guestLogin");
+        this._guestLoginButton.addEventListener("click", this._onGuestLogin);
         this._googleLoginButton = document.querySelector("#googleLogin");
+        this._guestLoginButton.addEventListener("click", this._onGoogleLogin);
 
         this._loadFeed();
     }
@@ -19,26 +23,22 @@ export default class App {
     async _loadFeed(){
         document.querySelector("#feed").textContent = "";
         let data = await apiRequest("GET", "/feed");
-        let posts = data.posts.map((postData) => new Post(postData));
-        for (let post of posts) {
-            this._displayPost(post);
+        for (let postData of data.posts) {
+            let elem = document.querySelector("#templatePost").cloneNode(true);
+            let post = new Post(postData, elem);
+
+            elem.classList.remove("hidden");
+            elem.id = "";
+
+            elem.querySelector(".postHeader").textContent = postData.header;
+            elem.querySelector(".postBody").textContent = postData.body;
+            elem.querySelector(".time").textContent = postData.time.toLocaleString();
+            elem.querySelector("#likeCount").textContent = postData.likeCount.toString();
+            elem.querySelector("#commentCount").textContent = postData.commentCount.toString();
+
+            document.querySelector("#feed").append(elem);
+            this._posts.push(post);
         }
-    }
-
-    _displayPost(post) {
-        if (!(post instanceof Post)) throw new Error("displayPost wasn't passed a Post object");
-
-        let elem = document.querySelector("#templatePost").cloneNode(true);
-        elem.classList.remove("hidden");
-        elem.id = "";
-
-        elem.querySelector(".postHeader").textContent = post.header;
-        elem.querySelector(".postBody").textContent = post.body;
-        elem.querySelector(".time").textContent = post.time.toLocaleString();
-        elem.querySelector("#likeCount").textContent = post.likeCount.toString();
-        elem.querySelector("#commentCount").textContent = post.commentCount.toString();
-
-        document.querySelector("#feed").append(elem);
     }
 
     /* handlers */
--- public/js/post.js
+++ public/js/post.js
@@ -1,10 +1,27 @@
-
 export default class Post {
-    constructor(data) {
+    constructor(data, postElm) {
         this.time = data.time;
         this.header = data.header;
         this.body = data.body;
         this.likeCount = data.likeCount;
         this.commentCount = data.commentCount;
+        this.post = postElm;   // DOM element
+
+        this._onLike = this._onLike.bind(this);
+        this._onComment = this._onComment.bind(this);
+
+        this._likeButton = this.post.querySelector(".likeButton");
+        this._likeButton.addEventListener("click", this._onLike);
+        this._commentButton = this.post.querySelector(".commentButton");
+        this._commentButton.addEventListener("click", this._onComment);
+    }
+
+    /* handlers */
+    async _onLike() {
+
+    }
+
+    async _onComment() {
+
     }
 }
